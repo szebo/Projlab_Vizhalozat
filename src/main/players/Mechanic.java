7c5ae388dff00f8e1a2a45f3053d8d4e0d63d855
Martin Sinka <sinka.martin@gmail.com>
2023-04-16 19:15:44 +0200

    mechanic proto

--- src/main/players/Mechanic.java
+++ src/main/players/Mechanic.java
@@ -10,19 +10,22 @@ public class Mechanic extends Player {
     private List<Pump> pumpsInInventory;
 
     public void pickUpPipe(Pipe pipe){
-
+        this.pipeInHand = Objects.requireNonNull(pipe, "Null értéket kapott a pipeInHand!");
     }
 
     public void placePipe(){
-
+        mapElement.attachPipe(pipeInHand);
+        pipeInHand = null; // atirni
     }
 
     public void pickUpPump(){
-
+       // pumpsInInventory.add(mapElement); ide kene valami praktikus. givePump()ot nem ismer a player
     }
 
     public void placePump(){
-
+        mapElement.
+        Pipe temp[] = mapElement.cut();
+        temp[1] =
     }
 
     public void repair(){
