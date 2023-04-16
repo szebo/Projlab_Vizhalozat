b5a5846fb4dd9ea7b2285137083d23999c4d8ead
TBoti <113096047+TBoti@users.noreply.github.com>
2023-04-17 01:55:22 +0200

    mechanicteamv1

--- src/main/players/MechanicTeam.java
+++ src/main/players/MechanicTeam.java
@@ -1,4 +1,17 @@
 package main.players;
 
+import main.map.Pump;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MechanicTeam {
+    private static MechanicTeam instance = null;
+    private final List<MechanicTeam> teamMembers = new ArrayList<MechanicTeam>() ;
+    private MechanicTeam(){}
+    public static synchronized MechanicTeam getInstance()
+    {
+        if (instance == null)
+            instance = new MechanicTeam();
+        return instance;
+    }
 }
