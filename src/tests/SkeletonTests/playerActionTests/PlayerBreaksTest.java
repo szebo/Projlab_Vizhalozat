3ab078fb71e7e9c8c77648964105f55d8f1d2b00
Németh Csongor <99328478+csnmth@users.noreply.github.com>
2023-04-22 10:47:42 +0200

    Szerelő can break Pipe
    MechanicBreaksPipe tesztfv., fájl és osztály átnevezése Saboteur -> Player

--- src/tests/SkeletonTests/playerActionTests/PlayerBreaksTest.java
+++ src/tests/SkeletonTests/playerActionTests/PlayerBreaksTest.java
@@ -3,7 +3,7 @@ package tests.SkeletonTests.playerActionTests;
 import main.map.*;
 import main.players.*;
 
-public class SaboteurBreaksTest {
+public class PlayerBreaksTest {
     /**
      * Létrejön egy szabotőr és egy cső
      * A szabotőrnek beállítjuk a mapElement értékét a csőre jelezve, hogy rálépett.
@@ -14,5 +14,18 @@ public class SaboteurBreaksTest {
         Pipe pipe = new Pipe();
         saboteur.setMapElement(pipe);
         saboteur.breakElement();
+    }
+    
+    /**
+     * Létrejön egy szerelő és egy cső
+     * A szerelőnek beállítjuk a mapElement értékét a csőre jelezve, hogy rálépett.
+     * A szerelő eltöri a csövet.
+     */
+    public static void MechanicBreaksPipe(){
+        Mechanic mechanic = new Mechanic();
+        Pipe pipe = new Pipe();
+        
+        mechanic.setMapElement(pipe);
+        mechanic.breakElement();
     }
 }
