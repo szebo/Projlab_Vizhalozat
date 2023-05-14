e5549986eade0b4ec57e24da3d1b027141c65321
szebo <szebo12@gmail.com>
2023-05-14 23:04:07 +0200

    estipush6

--- src/main/map/Pipe.java
+++ src/main/map/Pipe.java
@@ -129,9 +129,9 @@ public class Pipe extends MapElement implements Updatable {
             }
         }
         if(accepted)
-            Logger.log("console.txt", "["+getLogID()+"]: "+player.getLogID()+" accepted", true);
+            Logger.log("console.txt", "["+getLogID()+"]: "+player.getLogID()+" moved", true);
         else
-            Logger.log("console.txt", "["+getLogID()+"]: "+player.getLogID()+" was not accepted", true);
+            Logger.log("console.txt", "["+getLogID()+"]: "+player.getLogID()+" could not move", true);
         return accepted;
     }
 
