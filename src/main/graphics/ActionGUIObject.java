ac38ae023cf2693b7b07124849c3af2a9726429f
szebo <szebo12@gmail.com>
2023-05-22 17:09:37 +0200

    GUIObjectek váza, még a Swinges osztályból öröklés nincs meg

---
+++ src/main/graphics/ActionGUIObject.java
@@ -0,0 +1,23 @@
+package main.graphics;
+
+import java.awt.*;
+
+public class ActionGUIObject extends GUIObject{
+
+    public String guiMessage;
+
+    @Override
+    public void onClick() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public Point getPosition() {
+        return null;
+    }
+}
