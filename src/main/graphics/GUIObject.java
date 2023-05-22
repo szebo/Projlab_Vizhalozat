c77c3057e02c10e7fd5c616b2871ce5c1ffa57ab
szebo <szebo12@gmail.com>
2023-05-22 17:58:15 +0200

    GUIObject extends JComponent implements MouseListener

--- src/main/graphics/GUIObject.java
+++ src/main/graphics/GUIObject.java
@@ -2,16 +2,28 @@ package main.graphics;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public abstract class GUIObject extends AbstractButton implements ActionListener {
+public abstract class GUIObject extends JComponent implements MouseListener {
+
     @Override
-    public void actionPerformed(ActionEvent e){
+    public void mouseClicked(MouseEvent e){
         onClick();
     }
+
     public abstract void onClick();
     public void draw(){ this.repaint(); }
     public abstract Point getPosition();
 
+    @Override
+    public void mousePressed(MouseEvent e){}
+    @Override
+    public void mouseReleased(MouseEvent e){}
+
+    @Override
+    public void mouseEntered(MouseEvent e){}
+
+    @Override
+    public void mouseExited(MouseEvent e){}
 }
