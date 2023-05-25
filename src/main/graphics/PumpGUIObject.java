f0caf56ba41e911a6429a0ecc135e0d0cbdc1d32
konradnoel <konradnoel@gmail.com>
2023-05-25 22:45:18 +0200

    Pump drawBroken és drawWorks Saboteur és Mechanic drawStuck kommentelve? Spring drawAtPosition 90%

--- src/main/graphics/PumpGUIObject.java
+++ src/main/graphics/PumpGUIObject.java
@@ -29,15 +29,42 @@ public class PumpGUIObject extends GUIObject{
     public void draw(Graphics g){
         g.setColor(Color.BLACK);
         g.fillOval(position.x, position.y, 20, 20);
+        if(this.pump.isBroken()) drawBroken(position, g);
+        else drawWorking(position, g);
     }
 
-    private void drawWorking(Point point){
-
+    private void drawWorking(Point point, Graphics g){
+        g.setColor(Color.green);
+        g.fillOval(position.x, position.y, 5, 5);
     }
 
-    private void drawBroken(Point point){
+    private void drawBroken(Point point, Graphics g){
+        g.setColor(Color.red);
 
+        // Convert angle from degrees to radians
+        double angleRadians = Math.toRadians(45);
+
+        //Getting the first line's two ends
+        // Calculate the coordinate of the points of the first line
+        int point1X = (int) (point.x + 10 * Math.cos(angleRadians));
+        int point1Y = (int) (point.y + 10 * Math.sin(angleRadians));
+
+        int point2X = (int) (point.x - 10 * Math.cos(angleRadians));
+        int point2Y = (int) (point.y - 10 * Math.sin(angleRadians));
+
+        //Getting the second line's two ends
+        int point3X = (int) (point.x - 10 * Math.cos(angleRadians));
+        int point3Y = (int) (point.y + 10 * Math.sin(angleRadians));
+
+        int point4X = (int) (point.x + 10 * Math.cos(angleRadians));
+        int point4Y = (int) (point.y - 10 * Math.sin(angleRadians));
+
+
+        g.drawLine(point1X, point1Y, point2X, point2Y);
+        g.drawLine(point3X, point3Y, point4X, point4Y);
     }
+
+
 
     @Override
     public Point getPosition() {
--- src/main/graphics/SpringGUIObject.java
+++ src/main/graphics/SpringGUIObject.java
@@ -15,7 +15,7 @@ public class SpringGUIObject extends GUIObject{
     public SpringGUIObject(Spring spring){
         this.spring = spring;
         triangle = new Polygon();
-        triangle.addPoint(10, 0);
+        triangle.addPoint(7, 0);
         triangle.addPoint(0, 10);
         triangle.addPoint(14, 10);
     }
@@ -34,7 +34,24 @@ public class SpringGUIObject extends GUIObject{
     }
 
     public void drawAtPosition(Point point){
+        // Felső csúcs koordinátái
+        int sideLength = 20;
+        int bottomX = point.x;
+        int bottomY = point.y - sideLength;
+        Point bottomVertex = new Point(bottomX, bottomY);
 
+        // Bal alsó csúcs koordinátái
+        int leftX = point.x - sideLength/2;
+        int leftY = (int) (point.y + (sideLength/2) * Math.sqrt(3));
+        Point leftVertex = new Point(leftX, leftY);
+
+        // Jobb alsó csúcs koordinátái
+        int rightX = point.x + sideLength/2;
+        int rightY = (int) ( point.y + (sideLength/2) * Math.sqrt(3));
+        Point rightVertex = new Point(rightX, rightY);
+
+        //TODO Ezt nem tudom mikor hívjuk meg extrába, kell e neki paraméterként a Graphics objektum, meg stb,
+        //TODO de a három pontja megvan a háromszögnek, csak ki kell tölteni.
     }
 
     @Override
