4b3152b8c6c4d32a87f13621e88d8532ca3a420f
Martin Sinka <sinka.martin@gmail.com>
2023-05-30 22:10:46 +0200

    Endscreen + idk

--- src/main/windowing/EndScreen.java
+++ src/main/windowing/EndScreen.java
@@ -1,8 +1,38 @@
 package main.windowing;
 
+import main.players.MechanicTeam;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class EndScreen extends JPanel {
 
-    public EndScreen(){}
+    JButton bBack;
+    JLabel winnerTeam;
+
+
+    public EndScreen(Window window){
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        bBack = new JButton("Back To Menu");
+        bBack.addActionListener(window);
+
+        c.gridx = 0;
+        c.gridy = 0;
+
+        winnerTeam = new JLabel();
+        if(MechanicTeam.getInstance().hasWon()) {
+            winnerTeam.setText("Mechanic Team won!");
+            setBackground(Color.CYAN);
+        }
+        else {
+            winnerTeam.setText("Saboteur Team Won!");
+            setBackground(Color.ORANGE);
+        }
+        add(winnerTeam, c);
+        c.gridy = 1;
+        add(bBack, c);
+
+    }
 }
