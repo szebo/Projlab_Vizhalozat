1cfd4632ad56a6247f4a6d2376f689210104bdb7
szebo <szebo12@gmail.com>
2023-05-30 23:31:37 +0200

    betű méret

--- src/main/graphics/PipeGUIObject.java
+++ src/main/graphics/PipeGUIObject.java
@@ -146,14 +146,17 @@ public class PipeGUIObject extends GUIObject{
         //Státusz értékek a cső felett:
         if (pipe.checkSlippery()){
             g.setColor(Color.ORANGE);
+            g.setFont(new Font("Arial", Font.PLAIN,20));
             g.drawString(String.valueOf(pipe.getSlipperyFor()), getPosition().x, getPosition().y - 10);
         }
         if (pipe.checkSticky()){
             g.setColor(Color.GREEN);
+            g.setFont(new Font("Arial", Font.PLAIN,20));
             g.drawString(String.valueOf(pipe.getStickyFor()), getPosition().x - 5, getPosition().y - 10);
         }
         if (pipe.checkUnbreakable()){
             g.setColor(new Color(102,51, 0));
+            g.setFont(new Font("Arial", Font.PLAIN,20));
             g.drawString(String.valueOf(pipe.getUnbreakableFor()), getPosition().x + 5, getPosition().y - 10);
         }
 
