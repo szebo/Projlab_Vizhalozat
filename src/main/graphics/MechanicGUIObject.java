dbf54afa9e0ecaf02fdcbf6d74d3bd25d7c694e3
csnmth <nemethcsongor@edu.bme.hu>
2023-05-30 23:30:00 +0200

    drawstuck kész

--- src/main/graphics/MechanicGUIObject.java
+++ src/main/graphics/MechanicGUIObject.java
@@ -59,23 +59,24 @@ public class MechanicGUIObject extends GUIObject{
 
            //Getting the first line's two ends
            // Calculate the coordinate of the points of the first line
-           int point1X = (int) (point.x-5 + 20 * Math.cos(angleRadians));
-           int point1Y = (int) (point.y-5 + 20 * Math.sin(angleRadians));
+           int point1X = (int) (point.x + 14 * Math.cos(angleRadians));
+           int point1Y = (int) (point.y + 14 * Math.sin(angleRadians));
 
-           int point2X = (int) (point.x-5 - 20 * Math.cos(angleRadians));
-           int point2Y = (int) (point.y-5 - 20 * Math.sin(angleRadians));
+           int point2X = (int) (point.x - 14 * Math.cos(angleRadians));
+           int point2Y = (int) (point.y - 14 * Math.sin(angleRadians));
 
            //Getting the second line's two ends
-           int point3X = (int) (point.x-5 - 20 * Math.cos(angleRadians));
-           int point3Y = (int) (point.y-5 + 20 * Math.sin(angleRadians));
+           int point3X = (int) (point.x - 14 * Math.cos(angleRadians));
+           int point3Y = (int) (point.y + 14 * Math.sin(angleRadians));
 
-           int point4X = (int) (point.x-5 + 20 * Math.cos(angleRadians));
-           int point4Y = (int) (point.y-5 - 20 * Math.sin(angleRadians));
+           int point4X = (int) (point.x + 14 * Math.cos(angleRadians));
+           int point4Y = (int) (point.y - 14 * Math.sin(angleRadians));
 
-
+       Stroke tmp = g.getStroke();
        g.setStroke(new BasicStroke(2));
        g.drawLine(point1X, point1Y, point2X, point2Y);
        g.drawLine(point3X, point3Y, point4X, point4Y);
+       g.setStroke(tmp);
     }
 
     /**
--- src/main/graphics/SaboteurGUIObject.java
+++ src/main/graphics/SaboteurGUIObject.java
@@ -39,31 +39,33 @@ public class SaboteurGUIObject extends GUIObject{
     }
 
     public void drawStuck(Point point, Graphics2D g){
-            g.setColor(new Color(125, 125, 125));
+        g.setColor(new Color(125, 125, 125));
 
-            // Convert angle from degrees to radians
-            double angleRadians = Math.toRadians(45);
+        // Convert angle from degrees to radians
+        double angleRadians = Math.toRadians(45);
 
-            //Getting the first line's two ends
-            // Calculate the coordinate of the points of the first line
-            int point1X = (int) (point.x-10 + 20 * Math.cos(angleRadians));
-            int point1Y = (int) (point.y-10 + 20 * Math.sin(angleRadians));
+        //Getting the first line's two ends
+        // Calculate the coordinate of the points of the first line
+        int point1X = (int) (point.x + 14 * Math.cos(angleRadians));
+        int point1Y = (int) (point.y + 14 * Math.sin(angleRadians));
 
-            int point2X = (int) (point.x-10 - 20 * Math.cos(angleRadians));
-            int point2Y = (int) (point.y-10 - 20 * Math.sin(angleRadians));
+        int point2X = (int) (point.x - 14 * Math.cos(angleRadians));
+        int point2Y = (int) (point.y - 14 * Math.sin(angleRadians));
 
-            //Getting the second line's two ends
-            int point3X = (int) (point.x-10 - 20 * Math.cos(angleRadians));
-            int point3Y = (int) (point.y-10 + 20 * Math.sin(angleRadians));
+        //Getting the second line's two ends
+        int point3X = (int) (point.x - 14 * Math.cos(angleRadians));
+        int point3Y = (int) (point.y + 14 * Math.sin(angleRadians));
 
-            int point4X = (int) (point.x-10 + 20 * Math.cos(angleRadians));
-            int point4Y = (int) (point.y-10 - 20 * Math.sin(angleRadians));
+        int point4X = (int) (point.x + 14 * Math.cos(angleRadians));
+        int point4Y = (int) (point.y - 14 * Math.sin(angleRadians));
 
-            g.setStroke(new BasicStroke(2));
-            g.drawLine(point1X, point1Y, point2X, point2Y);
-            g.drawLine(point3X, point3Y, point4X, point4Y);
+        Stroke tmp = g.getStroke();
+        g.setStroke(new BasicStroke(2));
+        g.drawLine(point1X, point1Y, point2X, point2Y);
+        g.drawLine(point3X, point3Y, point4X, point4Y);
+        g.setStroke(tmp);
 
-     }
+    }
 
 
     public void drawAtPosition(Point point){
