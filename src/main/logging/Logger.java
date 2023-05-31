c88af078b7870dda3f4a08de4466e4518f5e3130
Martin Sinka <sinka.martin@gmail.com>
2023-05-31 02:00:29 +0200

    Logfile IOError fix

--- src/main/logging/Logger.java
+++ src/main/logging/Logger.java
@@ -10,20 +10,23 @@ import java.time.format.DateTimeFormatter;
 public class Logger {
     private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
-    public static void log(String file, String message, boolean console){
-        if(console) System.out.println(message);
-        if(Tester.currentTestLog != null && console){
-            Tester.currentTestLog.add(message);
+    public static void log(String file, String message, boolean console) {
+        if (console) {
+            System.out.println(message);
         }
-        else {
+
+        if (Tester.currentTestLog != null && console) {
+            Tester.currentTestLog.add(message);
+        } else {
             try {
                 LocalDateTime now = LocalDateTime.now();
-                File logFile = new File(Main.rootfolder + "/files/logs/" + file);
+                File logFile = new File(Main.rootfolder + File.separator + "files" + File.separator + "logs" + File.separator + file);
                 if (!logFile.exists()) {
-                    logFile.createNewFile();
+                    if(!logFile.getParentFile().mkdirs())throw new RuntimeException("Error while creating directories to logs.");
+                    if(logFile.createNewFile()) System.out.println("A log file was missing and created!");
                 }
                 BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true));
-                writer.append(dtf.format(now) + "\t");
+                writer.append(dtf.format(now)).append("\t");
                 writer.append(message).append("\n");
                 writer.close();
             } catch (IOException e) {
@@ -47,4 +50,6 @@ public class Logger {
             throw new RuntimeException(e);
         }
     }
+
+
 }
